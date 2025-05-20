8ce98eaadf725ebd4f6a138a7a6bd156f07e0cff
Aliaksei Sauko <alexeysju@gmail.com>
2025-05-21 00:22:42 +0300

    HW-3, added restaurant tabs

--- src/components/restaurants/index.jsx
+++ src/components/restaurants/index.jsx
@@ -1,18 +1,26 @@
+import 'react-tabs/style/react-tabs.css';
+import { TabList, Tabs, Tab, TabPanel } from 'react-tabs';
 import { Menu } from '../menu';
 import { Reviews } from '../reviews';
 
 export const Restaurants = ({ restaurants }) => {
   return (
     <div className='restaurants'>
-      {restaurants.map(({ id, name, menu, reviews }) => {
-        return (
-          <div className='restaurant' key={id}>
-            <h2>{name}</h2>
-            <Menu menuItems={menu}></Menu>
-            <Reviews reviews={reviews}></Reviews>
-          </div>
-        );
-      })}
+      <Tabs>
+        <TabList>
+          {restaurants.map(({ id, name }) => {
+            return <Tab key={id}>{name}</Tab>;
+          })}
+        </TabList>
+        {restaurants.map(({ id, menu, reviews }) => {
+          return (
+            <TabPanel key={id}>
+              <Menu menuItems={menu}></Menu>
+              <Reviews reviews={reviews}></Reviews>
+            </TabPanel>
+          );
+        })}
+      </Tabs>
     </div>
   );
 };
