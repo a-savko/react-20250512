7f43711438de46235a057e5263b45046f59ee337
Aliaksei Sauko <alexeysju@gmail.com>
2025-05-21 02:35:32 +0300

    HW-3, added Counter component

--- src/components/menu/index.jsx
+++ src/components/menu/index.jsx
@@ -1,10 +1,15 @@
+import { Counter } from './counter';
+
 export const Menu = ({ menuItems }) => {
   return (
     <div className='menu'>
       <h3>Menu</h3>
       <ul>
         {menuItems.map(({ id, name }) => (
-          <li key={id}>{name}</li>
+          <li key={id}>
+            {name}
+            <Counter></Counter>
+          </li>
         ))}
       </ul>
     </div>
---
+++ src/hooks/use-count.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+export const useCount = () => {
+    const [count, setCount] = useState(0);
+
+    const onDecrement = () => {
+        if (count > 0) {
+            setCount(count - 1);
+        }
+    };
+    const onIncrement = () => {
+        if (count < 5) {
+            setCount(count + 1);
+        }
+    };
+
+    return {
+        count,
+        onDecrement,
+        onIncrement,
+    };
+}
