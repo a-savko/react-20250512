e62e3df03a66cb0edb3b8ab98dfbf4d189f56294
Aliaksei Sauko <alexeysju@gmail.com>
2025-06-05 19:24:05 +0300

    HW-5, code style fixes, removed unnecessary code

--- src/components/buttons/default/button.jsx
+++ src/components/buttons/default/button.jsx
@@ -1,14 +1,8 @@
-import classNames from 'classnames';
-
 import styles from './button.module.css';
 
 export const Button = ({ children, onClick }) => {
   return (
-    <button
-      type='button'
-      className={classNames(styles.button)}
-      onClick={onClick}
-    >
+    <button type='button' className={styles.button} onClick={onClick}>
       {children}
     </button>
   );
