59a30a37645fdb5199371043ce4fafdf70a08778
Aliaksei Sauko <alexeysju@gmail.com>
2025-06-12 17:43:47 +0300

    HW-6, code style fixes, added useCallback for exported functions

--- src/components/contexts/account-context/account-context-provider.jsx
+++ src/components/contexts/account-context/account-context-provider.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import { AccountContext } from './account-context';
 
 export const AccountContextProvider = ({ children }) => {
@@ -8,17 +8,17 @@ export const AccountContextProvider = ({ children }) => {
     return user !== null;
   };
 
-  const login = (user) => {
+  const login = useCallback((user) => {
     if (!user) {
       return;
     }
 
     setUser(user);
-  };
+  }, []);
 
-  const logOut = () => {
+  const logOut = useCallback(() => {
     setUser(null);
-  };
+  }, []);
 
   return (
     <AccountContext value={{ user, login, logOut, isAuthorized }}>
