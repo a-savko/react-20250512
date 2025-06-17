288e61798130b81e5aa42fa55de3bbc70c8a846d
Aliaksei Sauko <alexeysju@gmail.com>
2025-06-18 00:45:01 +0300

    HW-8, updated a tab button component to use navigation links instead of buttons

--- src/components/buttons/tab-button/tab-button.jsx
+++ src/components/buttons/tab-button/tab-button.jsx
@@ -3,21 +3,23 @@ import styles from './tab-button.module.css';
 import { useContext } from 'react';
 import { ThemeContext } from '../../contexts/theme-context/theme-context';
 import { BLUE, GREEN } from '../../contexts/theme-context/theme-constants';
+import { NavLink } from 'react-router';
 
-export const TabButton = ({ title, isActive, onClick }) => {
+export const TabButton = ({ title, href }) => {
   const { theme } = useContext(ThemeContext);
 
   return (
-    <button
-      className={classNames(styles.tabButton, {
-        [styles.active]: isActive,
-        [styles.blue]: theme === BLUE,
-        [styles.green]: theme === GREEN,
-      })}
-      disabled={isActive}
-      onClick={onClick}
+    <NavLink
+      to={href}
+      className={({ isActive }) =>
+        classNames(styles.tabButton, {
+          [styles.active]: isActive,
+          [styles.blue]: theme === BLUE,
+          [styles.green]: theme === GREEN,
+        })
+      }
     >
       {title}
-    </button>
+    </NavLink>
   );
 };
