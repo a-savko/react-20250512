70f687cab1fca59848ccf42bdd230e57a42681b9
Aliaksei Sauko <alexeysju@gmail.com>
2025-06-18 21:37:38 +0300

    HW-8, layout fixes: footer alignment, navigation, themes

--- src/components/_layout/index.jsx
+++ src/components/_layout/index.jsx
@@ -1,41 +1,56 @@
 import { useContext } from 'react';
-import { ThemeButton } from '../buttons/theme-button/theme-button';
 import { ProgressBar } from '../progress-bar/progress-bar';
 import styles from './layout.module.css';
 import { ThemeContext } from '../contexts/theme-context/theme-context';
 import classNames from 'classnames';
 import { BLUE, GREEN } from '../contexts/theme-context/theme-constants';
-import { AccountContextProvider } from '../contexts/account-context/account-context-provider';
 import { Account } from '../account/account';
 import { Basket } from '../basket/basket';
 import { AccountContext } from '../contexts/account-context/account-context';
+import { Link, Outlet } from 'react-router';
+import { BackButton } from '../buttons/back-button/back-button';
+import { NavigationContext } from '../contexts/navigation-context/navigation-context';
+import { ROUTE_PATHS } from '../../constants/router-constants';
 
-export const Layout = ({ children }) => {
+export const Layout = ({ showTitle = true }) => {
   const { theme } = useContext(ThemeContext);
   const themeClassNames = {
     [styles.blue]: theme === BLUE,
     [styles.green]: theme === GREEN,
   };
   const { isAuthorized } = useContext(AccountContext);
+  const { isBackButtonHidden, backButtonLink, backButtonTitle } =
+    useContext(NavigationContext);
 
   return (
-    <>
+    <div className={classNames(styles.container, themeClassNames)}>
       <ProgressBar />
-      <header className={classNames(styles.header, themeClassNames)}>
-        <div>Food Delivery Service</div>
-        <div>
+      <header className={styles.header}>
+        <div className={styles.leftContainer}>
+          {!isBackButtonHidden && (
+            <BackButton to={backButtonLink} title={backButtonTitle} />
+          )}
+        </div>
+        <div className={styles.title}>
+          {showTitle && (
+            <Link to={ROUTE_PATHS.Home}>Express Food Delivery</Link>
+          )}
+        </div>
+        <div className={styles.rightContainer}>
           <Account />
         </div>
       </header>
-      {children}
+
+      <main className={styles.content}>
+        <Outlet />
+      </main>
+
       {isAuthorized && (
         <div>
           <Basket />
         </div>
       )}
-      <footer className={classNames(styles.footer, themeClassNames)}>
-        Footer
-      </footer>
-    </>
+      <footer className={styles.footer}>Footer</footer>
+    </div>
   );
 };
