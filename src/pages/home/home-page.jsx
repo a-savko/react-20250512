13449c75822720cdec28331090240df9a2e4c9ee
Aliaksei Sauko <alexeysju@gmail.com>
2025-06-18 21:32:22 +0300

    HW-8, added page Home

---
+++ src/pages/home/home-page.jsx
@@ -0,0 +1,31 @@
+import styles from './home-page.module.css';
+import { ReactSVG } from 'react-svg';
+import expressFoodDeliverySVG from '../../../public/express-food-delivery.svg';
+import { useContext } from 'react';
+import { ThemeContext } from '../../components/contexts/theme-context/theme-context';
+import classNames from 'classnames';
+import {
+  BLUE,
+  GREEN,
+} from '../../components/contexts/theme-context/theme-constants';
+import { OrderNowButton } from '../../components/buttons/order-now-button/order-now-button';
+import { ROUTE_PATHS } from '../../constants/router-constants';
+
+export const HomePage = () => {
+  const { theme } = useContext(ThemeContext);
+
+  return (
+    <div
+      className={classNames(styles.logo, {
+        [styles.blue]: theme === BLUE,
+        [styles.green]: theme === GREEN,
+      })}
+    >
+      <ReactSVG src={expressFoodDeliverySVG} />
+      <h3 className={styles.slogan}>
+        Get Your Favourite Meals Delivered Fast!
+      </h3>
+      <OrderNowButton title='Order Now' to={ROUTE_PATHS.Restaurants} />
+    </div>
+  );
+};
