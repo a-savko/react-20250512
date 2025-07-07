b2a62813e228eba9e757196855bd8717307a2cf2
Aliaksei Sauko <alexeysju@gmail.com>
2025-07-08 01:34:10 +0300

    HW-9, added api endpoint constants

---
+++ src/redux/constants/api-endpoint-constants.js
@@ -0,0 +1,10 @@
+
+export const API_PATHS = {
+    Restaurants: '/restaurants',
+    RestaurantDetails: `/restaurant/:restaurantId`,
+    RestaurantReviews: '/reviews',
+    Dishes: '/dishes',
+    DishDetails: `/dish/:dishId`,
+    Users: '/users',
+};
+
