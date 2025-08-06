27a2dc382266a71c1bfdfd02616afb50ab620648
Aliaksei Sauko <alexeysju@gmail.com>
2025-08-06 21:36:36 +0300

    HW-11, fixed favicon path

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -15,7 +15,7 @@ const RootLayout = ({ children }) => {
       <head>
         <meta charSet='UTF-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-        <link rel='icon' type='image/svg+xml' href='/public/react.svg' />
+        <link rel='icon' type='image/svg+xml' href='/react.svg' />
         <title>Food Delivery Service</title>
       </head>
       <body>
